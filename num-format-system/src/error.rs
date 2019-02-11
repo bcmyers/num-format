b1cc3d4378793e81d325055d1680e6b1e57c8768
Brian Myers <brian.carl.myers@gmail.com>
2019-02-10 22:42:28 -0500

    update

--- num-format-system/src/error.rs
+++ num-format-system/src/error.rs
@@ -5,35 +5,38 @@ pub struct Error;
 
 impl Error {
     /// TODO
-    pub fn new(_: &str) -> Error {
-        unimplemented!()
+    pub fn new(msg: &str) -> Error {
+        panic!("TODO: {}", msg)
     }
 
     #[allow(dead_code)]
     pub(crate) fn capacity(_: usize, _: usize) -> Error {
-        unimplemented!()
+        panic!("TODO: Capacity error")
     }
 
     #[allow(dead_code)]
-    pub(crate) fn unix(_: &str) -> Error {
-        unimplemented!()
+    pub(crate) fn unix(msg: &str) -> Error {
+        panic!("TODO: {}", msg)
     }
 
     #[allow(dead_code)]
-    pub(crate) fn windows<S>(_: S) -> Error where S: AsRef<str> {
-        unimplemented!()
+    pub(crate) fn windows<S>(msg: S) -> Error
+    where
+        S: AsRef<str>,
+    {
+        panic!("TODO: {}", msg.as_ref())
     }
 }
 
 impl From<std::ffi::NulError> for Error {
     fn from(_: std::ffi::NulError) -> Error {
-        unimplemented!()
+        panic!("TODO: std::ffi::NulError")
     }
 }
 
 #[cfg(windows)]
 impl From<widestring::NulError<u16>> for Error {
     fn from(_: widestring::NulError<u16>) -> Error {
-        unimplemented!()
+        panic!("TODO: widestring::NulError<u16>>")
     }
 }
