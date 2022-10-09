a572629bdf995da0d7a72c7217cfe3a58d11304b
Brian Myers <brian.carl.myers@gmail.com>
2022-10-09 15:17:25 -0400

    [num-format] Bump Windows deps

--- num-format/tests/test_system_locale_windows.rs
+++ num-format/tests/test_system_locale_windows.rs
@@ -7,6 +7,6 @@ fn test_windows() {
     let names = SystemLocale::available_names().unwrap();
     assert!(!names.is_empty());
     for name in &names {
-        let _ = SystemLocale::from_name(name.as_ref()).unwrap();
+        let _ = SystemLocale::from_name(name).unwrap();
     }
 }
