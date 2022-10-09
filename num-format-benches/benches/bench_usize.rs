08db2046e2506a4f83d7636bd4820b742d99edb3
Brian Myers <brian.carl.myers@gmail.com>
2022-10-09 12:52:23 -0400

    [num-format-benches] Bump criterion to 0.4.0

--- num-format-benches/benches/bench_usize.rs
+++ num-format-benches/benches/bench_usize.rs
@@ -2,88 +2,63 @@
 
 use std::time::Duration;
 
-use criterion::{criterion_group, criterion_main, Benchmark, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use num_format::{Buffer, Locale, ToFormattedString, WriteFormatted};
 
 fn bench_usize(c: &mut Criterion) {
     let measurement_time = Duration::from_millis(5_000); // Default is 5 seconds
     let sample_size = 100; // Default is 100
     let warm_up_time = Duration::from_millis(3_000); // Default is 3 seconds
+    let mut group = c.benchmark_group("usize");
 
-    c.bench(
-        "usize",
-        Benchmark::new("std/to_string/10_000", |b| {
-            b.iter(|| {
-                let s = 10_000usize.to_string();
-            })
+    group.bench_function("std/to_string/10_000", |b| {
+        b.iter(|| {
+            let s = 10_000usize.to_string();
         })
-        .with_function("itoa/buffer/10_000", |b| {
-            b.iter(|| {
-                let mut bytes = [b'\0'; 40];
-                let n = itoa::write(&mut bytes[..], 10_000usize).unwrap();
-            })
+    });
+    group.bench_function("num-format/buffer/10_000", |b| {
+        b.iter(|| {
+            let mut buf = Buffer::default();
+            buf.write_formatted(&10_000usize, &Locale::en);
         })
-        .with_function("itoa/write/10_000", |b| {
-            let mut s = String::new();
-            b.iter(|| {
-                itoa::fmt(&mut s, 10_000usize).unwrap();
-            })
+    });
+    group.bench_function("num-format/write/10_000", |b| {
+        let mut s = String::new();
+        b.iter(|| {
+            s.write_formatted(&10_000usize, &Locale::en).unwrap();
         })
-        .with_function("num-format/buffer/10_000", |b| {
-            b.iter(|| {
-                let mut buf = Buffer::default();
-                buf.write_formatted(&10_000usize, &Locale::en);
-            })
+    });
+    group.bench_function("num-format/to_string/10_000", |b| {
+        b.iter(|| {
+            let s = 10_000usize.to_formatted_string(&Locale::en);
         })
-        .with_function("num-format/write/10_000", |b| {
-            let mut s = String::new();
-            b.iter(|| {
-                s.write_formatted(&10_000usize, &Locale::en).unwrap();
-            })
+    });
+    group.bench_function("std/to_string/MAX", |b| {
+        b.iter(|| {
+            let s = std::usize::MAX.to_string();
         })
-        .with_function("num-format/to_string/10_000", |b| {
-            b.iter(|| {
-                let s = 10_000usize.to_formatted_string(&Locale::en);
-            })
+    });
+    group.bench_function("num-format/buffer/MAX", |b| {
+        b.iter(|| {
+            let mut buf = Buffer::default();
+            buf.write_formatted(&std::usize::MAX, &Locale::en);
         })
-        .with_function("std/to_string/MAX", |b| {
-            b.iter(|| {
-                let s = std::usize::MAX.to_string();
-            })
+    });
+    group.bench_function("num-format/write/MAX", |b| {
+        let mut s = String::new();
+        b.iter(|| {
+            s.write_formatted(&std::usize::MAX, &Locale::en).unwrap();
         })
-        .with_function("itoa/buffer/MAX", |b| {
-            b.iter(|| {
-                let mut bytes = [b'\0'; 40];
-                let n = itoa::write(&mut bytes[..], std::usize::MAX).unwrap();
-            })
+    });
+    group.bench_function("num-format/to_string/MAX", |b| {
+        b.iter(|| {
+            let s = std::usize::MAX.to_formatted_string(&Locale::en);
         })
-        .with_function("itoa/write/MAX", |b| {
-            let mut s = String::new();
-            b.iter(|| {
-                itoa::fmt(&mut s, std::usize::MAX).unwrap();
-            })
-        })
-        .with_function("num-format/buffer/MAX", |b| {
-            b.iter(|| {
-                let mut buf = Buffer::default();
-                buf.write_formatted(&std::usize::MAX, &Locale::en);
-            })
-        })
-        .with_function("num-format/write/MAX", |b| {
-            let mut s = String::new();
-            b.iter(|| {
-                s.write_formatted(&std::usize::MAX, &Locale::en).unwrap();
-            })
-        })
-        .with_function("num-format/to_string/MAX", |b| {
-            b.iter(|| {
-                let s = std::usize::MAX.to_formatted_string(&Locale::en);
-            })
-        })
-        .measurement_time(measurement_time)
-        .sample_size(sample_size)
-        .warm_up_time(warm_up_time),
-    );
+    });
+    group.measurement_time(measurement_time);
+    group.sample_size(sample_size);
+    group.warm_up_time(warm_up_time);
+    group.finish();
 }
 
 criterion_group! {
